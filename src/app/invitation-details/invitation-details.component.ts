70dffd8a235131ef60b1e2b8b6df24caae668c69
danieltalebi <danielt@yambay.com>
2019-07-25 18:13:21 +1000

    Template for Invitation Details working

--- src/app/invitation-details/invitation-details.component.ts
+++ src/app/invitation-details/invitation-details.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+
+declare const Waypoint: any;
 
 @Component({
   selector: 'app-invitation-details',
   templateUrl: './invitation-details.component.html',
-  styleUrls: ['./invitation-details.component.css', './materialize.css']
+  styleUrls: ['./invitation-details.component.css']
 })
 export class InvitationDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    const items = Array.from(document.getElementsByClassName('animate-box'));
+    for (const item of items) {
+      this.createWaypoint(item);
+    }
+
+  }
+
+  createWaypoint(item: Element) {
+    const waypoint = new Waypoint({
+      element: item,
+      handler: (direction) => {
+        console.log('The direction is ' + direction);
+        if (direction === 'down') {
+          item.classList.remove('fadeOutUp', 'animated');
+          item.classList.add('fadeInUp', 'animated');
+        } else {
+          item.classList.remove('fadeInUp', 'animated');
+          item.classList.add('fadeOutUp', 'animated');
+        }
+      },
+      offset: '95%'
+    });
   }
 
 }
