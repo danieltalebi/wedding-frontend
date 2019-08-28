0d234ec756dc7e8e8362859f4929352f343ba55e
danieltalebi <danielt@yambay.com>
2019-08-28 20:21:51 +1000

    Retrying request to backend and handling error

--- src/app/invitation-validator/invitations.service.ts
+++ src/app/invitation-validator/invitations.service.ts
@@ -1,8 +1,8 @@
 import { Invitation, Status } from './../model/Invitation';
 import {Injectable, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {map, catchError, retry} from 'rxjs/operators';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {map, catchError, retry, retryWhen, delay, take} from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { frontendCachedInvitations } from '../model/FrontendCachedInvitations';
 
@@ -54,6 +54,7 @@ export class InvitationsService implements OnInit {
     console.log('Fetching Invitations');
     return this.httpClient.get<any>(this.baseUrl + '/invitations?page=0&size=1000&projection=InvitationProjection', {})
     .pipe(
+      retryWhen(errors => errors.pipe(delay(5000), take(100))),
       map(response => {
         if (!!response._embedded.invitations) {
           let invitations: Invitation[] = response._embedded.invitations;
@@ -66,10 +67,25 @@ export class InvitationsService implements OnInit {
         }
         return [];
       }),
-      catchError(error => error.message || error)
+      catchError(this.handleError)
     );
   }
     
-    
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}` +
+        `message was: ${error.message}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };  
 }
   
